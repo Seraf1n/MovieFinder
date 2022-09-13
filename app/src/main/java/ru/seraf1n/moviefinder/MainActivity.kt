8496a1dc23d74e2c2f15535d18f94b1b5dad47e0
Gromtsev Yaroslav <ygromtsev@gmail.com>
2022-09-13 10:54:35 +0300

    Project Init

---
+++ app/src/main/java/ru/seraf1n/moviefinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.seraf1n.moviefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
