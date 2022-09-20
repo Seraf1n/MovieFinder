72a448a5616e9a65a18b2db75d98b1ad71665486
Gromtsev Yaroslav <ygromtsev@gmail.com>
2022-09-20 11:37:34 +0300

    Unit 20.8 View elements enhancement

--- app/src/main/java/ru/seraf1n/moviefinder/MainActivity.kt
+++ app/src/main/java/ru/seraf1n/moviefinder/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.seraf1n.moviefinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuButtons()
+    }
+
+    private fun initMenuButtons() {
+        findViewById<Button>(R.id.btnMenu).setOnClickListener {
+            Toast.makeText(this, "${(it as MaterialButton).text}", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnComp).setOnClickListener {
+            Toast.makeText(this, "${(it as MaterialButton).text}", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnFav).setOnClickListener {
+            Toast.makeText(this, "${(it as MaterialButton).text}", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnLater).setOnClickListener {
+            Toast.makeText(this, "${(it as MaterialButton).text}", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btnSettings).setOnClickListener {
+            Toast.makeText(this, "${(it as MaterialButton).text}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
