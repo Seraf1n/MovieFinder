4f35514ed373700e53382d64a81f82411890af69
Gromtsev Yaroslav <ygromtsev@gmail.com>
2022-11-08 11:45:00 +0300

    Добавлена болванка функционала избранного.

---
+++ app/src/main/java/ru/seraf1n/moviefinder/FavoritesFragment.kt
@@ -0,0 +1,57 @@
+package ru.seraf1n.moviefinder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_favorites.view.*
+import ru.seraf1n.moviefinder.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Получаем список при транзакции фрагмента
+        val favoritesList: List<Film> = emptyList()
+        binding.root.favorites_recycler
+            .apply {
+                filmsAdapter =
+                    FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                        override fun click(film: Film) {
+                            (requireActivity() as MainActivity).launchDetailsFragment(film)
+                        }
+                    })
+                //Присваиваем адаптер
+                adapter = filmsAdapter
+                //Присвои layoutmanager
+                layoutManager = LinearLayoutManager(requireContext())
+                //Применяем декоратор для отступов
+                val decorator = TopSpacingItemDecoration(8)
+                addItemDecoration(decorator)
+            }
+        //Кладем нашу БД в RV
+        filmsAdapter.addItems(favoritesList)
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/ru/seraf1n/moviefinder/Film.kt
+++ app/src/main/java/ru/seraf1n/moviefinder/Film.kt
@@ -4,5 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Film(val title: String, val poster: Int, val description: String):Parcelable {
-}
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String,
+    var isInFavorites: Boolean = false
+) : Parcelable
