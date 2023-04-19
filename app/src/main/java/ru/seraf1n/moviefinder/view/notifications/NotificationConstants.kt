e349d97d9082f29c55d2271a0e6bf1b7f033e602
Gromtsev Yaroslav <ygromtsev@gmail.com>
2023-04-19 09:57:36 +0300

    Модуль 48.7 Создание нотификации

--- app/src/main/java/ru/seraf1n/moviefinder/view/notifications/NotificationConstants.kt
+++ app/src/main/java/ru/seraf1n/moviefinder/view/notifications/NotificationConstants.kt
@@ -2,4 +2,5 @@ package ru.seraf1n.moviefinder.view.notifications
 
 object NotificationConstants {
     const val CHANNEL_ID = "FilmsSearchChannel"
+    const val POSTER_NOTIFICATION_DETAILED = "w500"
 }
